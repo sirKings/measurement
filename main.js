5079da6ac8c64806473f2ba1033d953b8f95155f
Kingsley <nwankwo.kingsleyjohn@gmail.com>
2017-07-05 12:33:07 +0100

    merging two platform

---
+++ main.js
@@ -0,0 +1,183 @@
+
+
+
+var generate = document.querySelector("#generate");
+
+generate.addEventListener("click", function (){
+
+//grab the input values from the user
+
+var shoulderFront = document.querySelector("#shoulderFront").value;
+var shoulderSide = document.querySelector("#shoulderSide").value;
+document.querySelector("#shoulderValue").value = shoulderFront;
+
+var sleeveFront = document.querySelector("#sleeveFront").value;
+var sleeveSide = document.querySelector("#sleeveSide").value;
+document.querySelector("#sleeveValue").value = Number(sleeveFront) + Number(sleeveSide);
+
+var bicepFront = document.querySelector("#bicepFront").value;
+var bicepSide = document.querySelector("#bicepSide").value;
+document.querySelector("#bicepValue").value = valueCalc(bicepFront,bicepSide);
+
+var wristFront = document.querySelector("#wristFront").value;
+var wristSide = document.querySelector("#wristSide").value;
+document.querySelector("#wristValue").value = valueCalc(wristFront,wristSide);
+
+
+var stomachFront = document.querySelector("#stomachFront").value;
+var stomachSide = document.querySelector("#stomachSide").value;
+document.querySelector("#stomachValue").value = valueCalc(stomachFront,stomachSide);
+
+var waistBellyFront = document.querySelector("#waistBellyFront").value;
+var waistBellySide = document.querySelector("#waistBellySide").value;
+document.querySelector("#waistBellyValue").value = valueCalc(waistBellyFront,waistBellySide);
+
+var hipsFront = document.querySelector("#hipsFront").value;
+var hipsSide = document.querySelector("#hipsSide").value;
+document.querySelector("#hipsValue").value = valueCalc(hipsFront,hipsSide);
+
+var frontJacketFront = document.querySelector("#frontJacketFront").value;
+var frontJacketSide = document.querySelector("#frontJacketSide").value;
+document.querySelector("#frontJacketValue").value = frontJacketFront;
+
+var chestFront = document.querySelector("#chestFront").value;
+var chestSide = document.querySelector("#chestSide").value;
+document.querySelector("#chestValue").value = valueCalc(chestFront,chestSide);
+
+var halfShoulderFront = document.querySelector("#halfShoulderFront").value;
+var halfShoulderSide = document.querySelector("#halfShoulderSide").value;
+document.querySelector("#halfShoulderValue").value = halfShoulderFront;
+
+var shoulderFront = document.querySelector("#shoulderFront").value;
+var shoulderSide = document.querySelector("#shoulderSide").value;
+document.querySelector("#shoulderValue").value = shoulderFront;
+
+var fullBackFront = document.querySelector("#fullBackFront").value;
+var fullBackSide = document.querySelector("#fullBackSide").value;
+document.querySelector("#fullBackValue").value = fullBackFront;
+
+var halfBackFront = document.querySelector("#halfBackFront").value;
+var halfBackSide = document.querySelector("#halfBackSide").value;
+document.querySelector("#halfBackValue").value = halfBackFront;
+
+var trouserWaistFront = document.querySelector("#trouserWaistFront").value;
+var trouserWaistSide = document.querySelector("#trouserWaistSide").value;
+document.querySelector("#trouserWaistValue").value = valueCalc(trouserWaistFront,trouserWaistSide);
+
+var trouserOutseamFront = document.querySelector("#trouserOutseamFront").value;
+var trouserOutseamSide = document.querySelector("#trouserOutseamSide").value;
+document.querySelector("#trouserOutseamValue").value = trouserOutseamFront;
+
+var trouserInseamFront = document.querySelector("#trouserInseamFront").value;
+var trouserInseamSide = document.querySelector("#trouserInseamSide").value;
+document.querySelector("#trouserInseamValue").value = trouserInseamFront;
+
+var crotchFront = document.querySelector("#crotchFront").value;
+var crotchSide = document.querySelector("#crotchSide").value;
+document.querySelector("#crotchValue").value = crotchFront;
+
+var thighFront = document.querySelector("#thighFront").value;
+var thighSide = document.querySelector("#thighSide").value;
+document.querySelector("#thighValue").value = valueCalc(thighFront,thighSide);
+ 	
+var kneeFront = document.querySelector("#kneeFront").value;
+var kneeSide = document.querySelector("#kneeSide").value;
+document.querySelector("#kneeValue").value = valueCalc(kneeFront,kneeSide);
+
+
+ })
+
+//grab the refresh button
+var refresh = document.querySelector("#refresh");
+
+//set the button to listen for clicks
+
+refresh.addEventListener("click", function(){
+
+//change the value of all the input boxes back to empty. 
+document.querySelector("#shoulderFront").value = "";
+document.querySelector("#shoulderSide").value = "";
+document.querySelector("#shoulderValue").value = shoulderFront;
+
+document.querySelector("#sleeveFront").value = "";
+document.querySelector("#sleeveSide").value = "";
+document.querySelector("#sleeveValue").value = "";
+
+document.querySelector("#bicepFront").value = "";
+document.querySelector("#bicepSide").value = "";
+document.querySelector("#bicepValue").value = "";
+
+document.querySelector("#wristFront").value = "";
+document.querySelector("#wristSide").value = "";
+document.querySelector("#wristValue").value = "";
+
+
+document.querySelector("#stomachFront").value = "";
+document.querySelector("#stomachSide").value = "";
+document.querySelector("#stomachValue").value = "";
+
+document.querySelector("#waistBellyFront").value = "";
+document.querySelector("#waistBellySide").value = "";
+document.querySelector("#waistBellyValue").value = "";
+
+document.querySelector("#hipsFront").value = "";
+document.querySelector("#hipsSide").value = "";
+document.querySelector("#hipsValue").value = "";
+
+document.querySelector("#frontJacketFront").value = "";
+document.querySelector("#frontJacketSide").value = "";
+document.querySelector("#frontJacketValue").value = "";
+
+document.querySelector("#chestFront").value = "";
+document.querySelector("#chestSide").value = "";
+document.querySelector("#chestValue").value = "";
+
+document.querySelector("#halfShoulderFront").value = "";
+document.querySelector("#halfShoulderSide").value = "";
+document.querySelector("#halfShoulderValue").value = "";
+
+document.querySelector("#shoulderFront").value = "";
+document.querySelector("#shoulderSide").value = "";
+document.querySelector("#shoulderValue").value = "";
+
+document.querySelector("#fullBackFront").value = "";
+document.querySelector("#fullBackSide").value = "";
+document.querySelector("#fullBackValue").value = "";
+
+document.querySelector("#halfBackFront").value = "";
+document.querySelector("#halfBackSide").value = "";
+document.querySelector("#halfBackValue").value = "";
+
+document.querySelector("#trouserWaistFront").value = "";
+document.querySelector("#trouserWaistSide").value = "";
+document.querySelector("#trouserWaistValue").value = "";
+
+document.querySelector("#trouserOutseamFront").value = "";
+document.querySelector("#trouserOutseamSide").value = "";
+document.querySelector("#trouserOutseamValue").value = "";
+
+document.querySelector("#trouserInseamFront").value = "";
+document.querySelector("#trouserInseamSide").value = "";
+document.querySelector("#trouserInseamValue").value = "";
+
+document.querySelector("#crotchFront").value = "";
+document.querySelector("#crotchSide").value = "";
+document.querySelector("#crotchValue").value = "";
+
+document.querySelector("#thighFront").value = "";
+document.querySelector("#thighSide").value = "";
+document.querySelector("#thighValue").value = "";
+ 	
+document.querySelector("#kneeFront").value = "";
+document.querySelector("#kneeSide").value = "";
+document.querySelector("#kneeValue").value = "";
+})
+
+
+
+//function to calculate circumference of an ellipse, ie length of elliptical parameters.
+function valueCalc(a , b){
+	var val = (Math.sqrt(((a/2 * a/2) + (b/2 * b/2)) / 2)) * 6.284;
+	return val;
+}
+
